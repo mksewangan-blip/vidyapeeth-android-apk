5b7a394f577922711f8e503aebcf8950abbee5f0
Sewangan <mksewangan@gmail.com>
2026-09-01 13:18:50 +0530

    Delete app directory

--- app/src/main/assets/www/assets/js/android-bridge.js
+++
@@ -1,5 +0,0 @@
-(function(){
-  window.SVAndroidNotify=function(title,message){
-    try{ if(window.VidyapeethAndroid && typeof window.VidyapeethAndroid.notify==='function') window.VidyapeethAndroid.notify(String(title||'Sewangan Vidyapeeth'),String(message||'')); }catch(e){}
-  };
-})();
